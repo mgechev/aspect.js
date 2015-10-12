404014085287e673a8b1a45101b4a68b7af5a457
mgechev <mgechev@gmail.com>
2015-10-12 13:51:24 +0300

    Update build

--- dist/test/advices/sync_advices.spec.js
+++ dist/test/advices/sync_advices.spec.js
@@ -7,151 +7,142 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
-(function (deps, factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define(deps, factory);
-    }
-})(["require", "exports", '../../lib/src/core', '../../lib/aspect', 'chai'], function (require, exports) {
-    var core_1 = require('../../lib/src/core');
-    var aspect_1 = require('../../lib/aspect');
-    var chai_1 = require('chai');
-    describe('sync advices', function () {
-        beforeEach(function () {
-            core_1.resetRegistry();
+var core_1 = require('../../lib/src/core');
+var aspect_1 = require('../../lib/aspect');
+var chai_1 = require('chai');
+describe('sync advices', function () {
+    beforeEach(function () {
+        core_1.resetRegistry();
+    });
+    describe('BeforeAdvice', function () {
+        // beforeEach(() => {
+        //   resetRegistry();
+        // });
+        it('should invoke the advice with the appropriate metadata', function (done) {
+            var demo;
+            var Aspect = (function () {
+                function Aspect() {
+                }
+                Aspect.prototype.before = function (metadata) {
+                    chai_1.expect(this).to.deep.equal(Aspect.prototype);
+                    chai_1.expect(metadata.method.context).to.eq(demo);
+                    chai_1.expect(metadata.className).to.equal('Demo');
+                    chai_1.expect(metadata.method.name).to.equal('get');
+                    chai_1.expect(metadata.method.args).to.deep.equal([42, 1.618]);
+                    done();
+                };
+                Object.defineProperty(Aspect.prototype, "before",
+                    __decorate([
+                        aspect_1.beforeMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
+                    ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
+                return Aspect;
+            })();
+            var Demo = (function () {
+                function Demo() {
+                }
+                Demo.prototype.get = function (foo, bar) { };
+                Demo = __decorate([
+                    core_1.Wove()
+                ], Demo);
+                return Demo;
+            })();
+            demo = new Demo();
+            demo.get(42, 1.618);
         });
-        describe('BeforeAdvice', function () {
-            // beforeEach(() => {
-            //   resetRegistry();
-            // });
-            it('should invoke the advice with the appropriate metadata', function (done) {
-                var demo;
-                var Aspect = (function () {
-                    function Aspect() {
-                    }
-                    Aspect.prototype.before = function (metadata) {
-                        chai_1.expect(this).to.deep.equal(Aspect.prototype);
-                        chai_1.expect(metadata.method.context).to.eq(demo);
-                        chai_1.expect(metadata.className).to.equal('Demo');
-                        chai_1.expect(metadata.method.name).to.equal('get');
-                        chai_1.expect(metadata.method.args).to.deep.equal([42, 1.618]);
-                        done();
-                    };
-                    Object.defineProperty(Aspect.prototype, "before",
-                        __decorate([
-                            aspect_1.beforeMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
-                        ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
-                    return Aspect;
-                })();
-                var Demo = (function () {
-                    function Demo() {
-                    }
-                    Demo.prototype.get = function (foo, bar) { };
-                    Demo = __decorate([
-                        core_1.Wove()
-                    ], Demo);
-                    return Demo;
-                })();
-                demo = new Demo();
-                demo.get(42, 1.618);
-            });
-            it('should invoke the advice before the target', function (done) {
-                var adviceCalled = false;
-                var methodCalled = false;
-                var Aspect = (function () {
-                    function Aspect() {
-                    }
-                    Aspect.prototype.before = function (metadata) {
-                        adviceCalled = true;
-                        chai_1.expect(methodCalled).to.equal(false);
-                    };
-                    Object.defineProperty(Aspect.prototype, "before",
-                        __decorate([
-                            aspect_1.beforeMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
-                        ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
-                    return Aspect;
-                })();
-                var Demo = (function () {
-                    function Demo() {
-                    }
-                    Demo.prototype.get = function () {
-                        methodCalled = true;
-                        chai_1.expect(adviceCalled).to.equal(true);
-                        done();
-                    };
-                    Demo = __decorate([
-                        core_1.Wove()
-                    ], Demo);
-                    return Demo;
-                })();
-                new Demo().get();
-            });
+        it('should invoke the advice before the target', function (done) {
+            var adviceCalled = false;
+            var methodCalled = false;
+            var Aspect = (function () {
+                function Aspect() {
+                }
+                Aspect.prototype.before = function (metadata) {
+                    adviceCalled = true;
+                    chai_1.expect(methodCalled).to.equal(false);
+                };
+                Object.defineProperty(Aspect.prototype, "before",
+                    __decorate([
+                        aspect_1.beforeMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
+                    ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
+                return Aspect;
+            })();
+            var Demo = (function () {
+                function Demo() {
+                }
+                Demo.prototype.get = function () {
+                    methodCalled = true;
+                    chai_1.expect(adviceCalled).to.equal(true);
+                    done();
+                };
+                Demo = __decorate([
+                    core_1.Wove()
+                ], Demo);
+                return Demo;
+            })();
+            new Demo().get();
         });
-        describe('AfterAdvice', function () {
-            it('should invoke the advice with the appropriate metadata', function (done) {
-                var demo;
-                var Aspect = (function () {
-                    function Aspect() {
-                    }
-                    Aspect.prototype.before = function (metadata) {
-                        chai_1.expect(this).to.deep.equal(Aspect.prototype);
-                        chai_1.expect(metadata.method.context).to.eq(demo);
-                        chai_1.expect(metadata.className).to.equal('Demo');
-                        chai_1.expect(metadata.method.name).to.equal('get');
-                        chai_1.expect(metadata.method.args).to.deep.equal([42, 1.618]);
-                        done();
-                    };
-                    Object.defineProperty(Aspect.prototype, "before",
-                        __decorate([
-                            aspect_1.afterMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
-                        ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
-                    return Aspect;
-                })();
-                var Demo = (function () {
-                    function Demo() {
-                    }
-                    Demo.prototype.get = function (foo, bar) { };
-                    Demo = __decorate([
-                        core_1.Wove()
-                    ], Demo);
-                    return Demo;
-                })();
-                demo = new Demo();
-                demo.get(42, 1.618);
-            });
-            it('should invoke the advice after the target', function (done) {
-                var adviceCalled = false;
-                var methodCalled = false;
-                var Aspect = (function () {
-                    function Aspect() {
-                    }
-                    Aspect.prototype.before = function (metadata) {
-                        adviceCalled = true;
-                        chai_1.expect(methodCalled).to.equal(true);
-                    };
-                    Object.defineProperty(Aspect.prototype, "before",
-                        __decorate([
-                            aspect_1.afterMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
-                        ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
-                    return Aspect;
-                })();
-                var Demo = (function () {
-                    function Demo() {
-                    }
-                    Demo.prototype.get = function () {
-                        methodCalled = true;
-                        chai_1.expect(adviceCalled).to.equal(false);
-                        done();
-                    };
-                    Demo = __decorate([
-                        core_1.Wove()
-                    ], Demo);
-                    return Demo;
-                })();
-                new Demo().get();
-            });
+    });
+    describe('AfterAdvice', function () {
+        it('should invoke the advice with the appropriate metadata', function (done) {
+            var demo;
+            var Aspect = (function () {
+                function Aspect() {
+                }
+                Aspect.prototype.before = function (metadata) {
+                    chai_1.expect(this).to.deep.equal(Aspect.prototype);
+                    chai_1.expect(metadata.method.context).to.eq(demo);
+                    chai_1.expect(metadata.className).to.equal('Demo');
+                    chai_1.expect(metadata.method.name).to.equal('get');
+                    chai_1.expect(metadata.method.args).to.deep.equal([42, 1.618]);
+                    done();
+                };
+                Object.defineProperty(Aspect.prototype, "before",
+                    __decorate([
+                        aspect_1.afterMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
+                    ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
+                return Aspect;
+            })();
+            var Demo = (function () {
+                function Demo() {
+                }
+                Demo.prototype.get = function (foo, bar) { };
+                Demo = __decorate([
+                    core_1.Wove()
+                ], Demo);
+                return Demo;
+            })();
+            demo = new Demo();
+            demo.get(42, 1.618);
+        });
+        it('should invoke the advice after the target', function (done) {
+            var adviceCalled = false;
+            var methodCalled = false;
+            var Aspect = (function () {
+                function Aspect() {
+                }
+                Aspect.prototype.before = function (metadata) {
+                    adviceCalled = true;
+                    chai_1.expect(methodCalled).to.equal(true);
+                };
+                Object.defineProperty(Aspect.prototype, "before",
+                    __decorate([
+                        aspect_1.afterMethod({ classNamePattern: /.*/, methodNamePattern: /.*/ })
+                    ], Aspect.prototype, "before", Object.getOwnPropertyDescriptor(Aspect.prototype, "before")));
+                return Aspect;
+            })();
+            var Demo = (function () {
+                function Demo() {
+                }
+                Demo.prototype.get = function () {
+                    methodCalled = true;
+                    chai_1.expect(adviceCalled).to.equal(false);
+                    done();
+                };
+                Demo = __decorate([
+                    core_1.Wove()
+                ], Demo);
+                return Demo;
+            })();
+            new Demo().get();
         });
     });
 });
--- dist/test/core/pointcut.spec.js
+++ dist/test/core/pointcut.spec.js
@@ -4,70 +4,61 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-(function (deps, factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+var core_1 = require('../../lib/src/core');
+var chai_1 = require('chai');
+var SimpleJP = (function (_super) {
+    __extends(SimpleJP, _super);
+    function SimpleJP() {
+        _super.apply(this, arguments);
     }
-    else if (typeof define === 'function' && define.amd) {
-        define(deps, factory);
-    }
-})(["require", "exports", '../../lib/src/core', 'chai'], function (require, exports) {
-    var core_1 = require('../../lib/src/core');
-    var chai_1 = require('chai');
-    var SimpleJP = (function (_super) {
-        __extends(SimpleJP, _super);
-        function SimpleJP() {
-            _super.apply(this, arguments);
+    SimpleJP.prototype.match = function (descriptor) {
+        if (this.precondition.assert(descriptor)) {
+            return [1];
         }
-        SimpleJP.prototype.match = function (descriptor) {
-            if (this.precondition.assert(descriptor)) {
-                return [1];
-            }
+        return [];
+    };
+    SimpleJP.prototype.wove = function () { };
+    return SimpleJP;
+})(core_1.JointPoint);
+var SimplePrecondition = (function () {
+    function SimplePrecondition() {
+    }
+    SimplePrecondition.prototype.assert = function (data) {
+        return true;
+    };
+    return SimplePrecondition;
+})();
+// TODO(mgechev) refactor with spies
+describe('Pointcut', function () {
+    var pc;
+    var jp1;
+    var jp2;
+    beforeEach(function () {
+        pc = new core_1.Pointcut();
+        jp1 = new SimpleJP(new SimplePrecondition());
+        jp1.precondition = new SimplePrecondition();
+        jp2 = new SimpleJP(new SimplePrecondition());
+        jp2.precondition = new SimplePrecondition();
+        pc.jointPoints = [jp1, jp2];
+    });
+    it('Match should be invoked', function (done) {
+        var bak = jp1.match;
+        var called1 = false;
+        jp1.match = function (d) {
+            bak.call(this, d);
+            called1 = true;
             return [];
         };
-        SimpleJP.prototype.wove = function () { };
-        return SimpleJP;
-    })(core_1.JointPoint);
-    var SimplePrecondition = (function () {
-        function SimplePrecondition() {
-        }
-        SimplePrecondition.prototype.assert = function (data) {
-            return true;
+        var bak2 = jp1.match;
+        var called2 = false;
+        jp2.match = function (d) {
+            bak2.call(this, d);
+            called2 = true;
+            return [];
         };
-        return SimplePrecondition;
-    })();
-    // TODO(mgechev) refactor with spies
-    describe('Pointcut', function () {
-        var pc;
-        var jp1;
-        var jp2;
-        beforeEach(function () {
-            pc = new core_1.Pointcut();
-            jp1 = new SimpleJP(new SimplePrecondition());
-            jp1.precondition = new SimplePrecondition();
-            jp2 = new SimpleJP(new SimplePrecondition());
-            jp2.precondition = new SimplePrecondition();
-            pc.jointPoints = [jp1, jp2];
-        });
-        it('Match should be invoked', function (done) {
-            var bak = jp1.match;
-            var called1 = false;
-            jp1.match = function (d) {
-                bak.call(this, d);
-                called1 = true;
-                return [];
-            };
-            var bak2 = jp1.match;
-            var called2 = false;
-            jp2.match = function (d) {
-                bak2.call(this, d);
-                called2 = true;
-                return [];
-            };
-            pc.apply({});
-            chai_1.expect(called1).to.equal(true);
-            chai_1.expect(called2).to.equal(true);
-            done();
-        });
+        pc.apply({});
+        chai_1.expect(called1).to.equal(true);
+        chai_1.expect(called2).to.equal(true);
+        done();
     });
 });
