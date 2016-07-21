f3a56e4b8252121e27c29a8a9b5353af21b5bef6
mgechev <mgechev@gmail.com>
2016-07-21 18:55:12 +0300

    fix: wove targets only once

--- lib/src/core/wove.ts
+++ lib/src/core/wove.ts
@@ -2,10 +2,14 @@ import {AspectRegistry} from './aspect';
 
 export function Wove(config?: any) {
   return function (target) {
+    if (target.__woven__) {
+      return;
+    }
     let keys = Object.getOwnPropertyNames(AspectRegistry);
     keys.forEach(key => {
       AspectRegistry[key].wove(target, config);
     });
+    target.__woven__ = true;
     return target;
   };
 }
