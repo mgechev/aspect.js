344a4b8c046bbf96fdc1887830913d46fc37ea77
mgechev <mgechev@gmail.com>
2017-05-30 22:04:56 -0400

    fix(pointcut): do not apply the same advice twice
    Fix #40

--- lib/src/core/pointcut.ts
+++ lib/src/core/pointcut.ts
@@ -6,7 +6,14 @@ export class Pointcut {
 
   public advice: Advice;
 
+  private _applications = new Set<Function>();
+
   public apply(fn: Function, woveMetadata: any) {
+    if (this._applications.has(fn)) {
+      return;
+    } else {
+      this._applications.add(fn);
+    }
     this.jointPoints.forEach(jp => {
       let matches = jp.match(fn);
       jp.wove({ fn, matches, woveMetadata }, this.advice);
