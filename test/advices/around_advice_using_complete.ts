0c75b645faa817525415057c7285346e41fa50cf
Matthew Adams <matthew@matthewadams.me>
2018-11-14 18:27:24 -0600

    add test

---
+++ test/advices/around_advice_using_complete.ts
@@ -0,0 +1,33 @@
+import { Wove, resetRegistry, Metadata } from "../../lib/src/core";
+
+import { expect } from "chai";
+import { aroundMethod } from "../../lib";
+
+describe("around advices", () => {
+  afterEach(() => {
+    resetRegistry();
+  });
+
+  it("should only get invoked once when using MethodMetdata.complete()", () => {
+    let count = 0;
+
+    class Aspect {
+      @aroundMethod({ classNamePattern: /^Test/, methodNamePattern: /^get$/ })
+      aroundMethod(meta: Metadata) {
+        return meta.method.complete(...meta.method.args) + " appended from the aspect";
+      }
+    }
+
+    @Wove()
+    class Test {
+      get(it: string): string {
+        count++;
+        return it;
+      }
+    }
+
+    const test = new Test();
+    expect(test.get("test")).to.equal("test appended from the aspect");
+    expect(count).to.equal(1);
+  });
+});
